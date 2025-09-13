b4100ef75b4b56d205eab12acb68d8adf11170ed
= <=>
2025-09-13 17:42:16 -0500

    feature/update-sidebar  and navbar

--- src/app/layout-page/layout-page.component.ts
+++ src/app/layout-page/layout-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidenavComponent } from "../shared/sidenav/sidenav.component";
+import {NavbarComponent} from '../shared/navbar/navbar.component';
 
 
 @Component({
   selector: 'app-layout-page',
-  imports: [RouterOutlet, SidenavComponent],
+  imports: [RouterOutlet, SidenavComponent, NavbarComponent],
   templateUrl: './layout-page.component.html',
 })
 export class LayoutPageComponent { }
